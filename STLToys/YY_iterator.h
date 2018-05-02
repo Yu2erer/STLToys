3c88ea8687a8c6591269f8261be88dee422e9e28
Yu2erer <yu2erer@gmail.com>
2018-05-02 18:53:22 +0800

    添加 YY_iterator

---
+++ STLToys/YY_iterator.h
@@ -0,0 +1,124 @@
+//
+//  YY_iterator.h
+//  STLToys
+//
+//  Created by ntian on 2018/5/2.
+//  Copyright © 2018 ntian. All rights reserved.
+//
+
+#ifndef STLTOYS_YY_ITERATOR_H
+#define STLTOYS_YY_ITERATOR_H
+
+#include <cstddef>
+
+namespace YY {
+
+    struct input_iterator_tag { };
+    struct output_iterator_tag { };
+    struct forward_iterator_tag : public input_iterator_tag { };
+    struct bidirectional_iterator_tag : public forward_iterator_tag { };
+    struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+
+    template <typename Category, typename T,
+            typename Distance = ptrdiff_t,
+            typename Pointer = T*, typename Reference = T& >
+    struct iterator {
+        typedef Category    iterator_category;
+        typedef T           value_type;
+        typedef Distance    difference_type;
+        typedef Pointer     pointer;
+        typedef Reference   reference;
+    };
+
+    template <typename Iterator>
+    struct iterator_traits {
+        typedef typename Iterator::iterator_category    iterator_category;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+    };
+
+    template <typename T>
+    struct iterator_traits<T*> {
+        typedef random_access_iterator_tag iterator_category;
+        typedef T                          value_type;
+        typedef ptrdiff_t                  difference_type;
+        typedef T*                         pointer;
+        typedef T&                         reference;
+    };
+
+    template <typename T>
+    struct iterator_traits<const T*> {
+        typedef random_access_iterator_tag iterator_category;
+        typedef T                          value_type;
+        typedef ptrdiff_t                  difference_type;
+        typedef const T*                   pointer;
+        typedef const T&                   reference;
+    };
+
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::iterator_category iterator_category(const Iterator&) {
+        typedef typename iterator_traits<Iterator>::iterator_category category;
+        return category();
+    }
+
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::value_type* value_type(const Iterator&) {
+        return static_cast<typedef typename iterator_traits<Iterator>::value_type*>(0);
+    }
+
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::difference_type* distance_type(const Iterator&) {
+        return static_cast<typedef typename iterator_traits<Iterator>::difference_type*>(0);
+    }
+
+    template <typename InputIterator>
+    inline iterator_traits<InputIterator>::difference_type __distance(InputIterator first, InputIterator last, input_iterator_tag) {
+        iterator_traits<InputIterator>::difference_type n = 0;
+        while (first != last) {
+            ++first;
+            ++n;
+        }
+        return n;
+    }
+
+    template <typename RandomAccessIterator>
+    inline iterator_traits<RandomAccessIterator>::difference_type __distance(RandomAccessIterator first, RandomAccessIterator last, random_access_iterator_tag) {
+        return last - first;
+    }
+
+    template <typename InputIterator>
+    inline iterator_traits<InputIterator>::difference_type distance(InputIterator first, InputIterator last) {
+        typedef typename iterator_traits<InputIterator>::iterator_category category;
+        return __distance(first, last, category());
+    }
+
+    template <typename InputIterator, typename Distance>
+    inline void __advance(InputIterator &i, Distance n, input_iterator_tag) {
+        while (n--) { ++i; }
+    }
+
+    template <typename BidirectionalIterator, typename Distance>
+    inline void __advance(BidirectionalIterator &i, Distance n, bidirectional_iterator_tag) {
+        if (n >= 0) {
+            while (n--) { ++i; }
+        } else {
+            while (n++) { --i; }
+        }
+    }
+
+    template <typename RandomAccessIterator, typename Distance>
+    inline void __advance(RandomAccessIterator &i, Distance n, random_access_iterator_tag) {
+        i += n;
+    }
+
+    template <typename InputIterator, typename Distance>
+    inline void advance(InputIterator &i, Distance n) {
+        typedef typename iterator_traits<InputIterator>::iterator_category category;
+        __advance(i, n, category());
+        // 也可以是 __advance(i, n, iterator_category(i));
+    }
+
+}
+#endif //STLTOYS_YY_ITERATOR_H
