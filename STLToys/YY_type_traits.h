a40c26733cb5ef954cc5e8383c75e85833344baf
ntian <1179953947@qq.com>
2018-05-30 17:36:16 +0800

    修正 YY_type_traits 拼写错误

--- STLToys/YY_type_traits.h
+++ STLToys/YY_type_traits.h
@@ -21,7 +21,7 @@ namespace YY {
         typedef __true_type this_dummy_member_must_be_first;
         typedef __false_type has_trivial_default_constructor;
         typedef __false_type has_trivial_copy_constructor;
-        typedef __false_type has_trivial_assignment_constructor;
+        typedef __false_type has_trivial_assignment_operator;
         typedef __false_type has_trivial_destructor;
         typedef __false_type is_POD_type;
     };
@@ -31,7 +31,7 @@ namespace YY {
     struct __type_traits<T*> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -41,7 +41,7 @@ namespace YY {
     struct __type_traits<char> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -50,7 +50,7 @@ namespace YY {
     struct __type_traits<signed char> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -59,7 +59,7 @@ namespace YY {
     struct __type_traits<unsigned char> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -68,7 +68,7 @@ namespace YY {
     struct __type_traits<short> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -77,7 +77,7 @@ namespace YY {
     struct __type_traits<unsigned short> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -86,7 +86,7 @@ namespace YY {
     struct __type_traits<int> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -95,7 +95,7 @@ namespace YY {
     struct __type_traits<unsigned int> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -104,7 +104,7 @@ namespace YY {
     struct __type_traits<long> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -113,7 +113,7 @@ namespace YY {
     struct __type_traits<unsigned long> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -122,7 +122,7 @@ namespace YY {
     struct __type_traits<float> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -131,7 +131,7 @@ namespace YY {
     struct __type_traits<double> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
@@ -140,7 +140,7 @@ namespace YY {
     struct __type_traits<long double> {
         typedef __true_type has_trivial_default_constructor;
         typedef __true_type has_trivial_copy_constructor;
-        typedef __true_type has_trivial_assignment_constructor;
+        typedef __true_type has_trivial_assignment_operator;
         typedef __true_type has_trivial_destructor;
         typedef __true_type is_POD_type;
     };
